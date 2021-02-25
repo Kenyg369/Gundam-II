0dce39215359722b68a11162f8bfafb6291edafe
Kenyg369 <kengrand369@gmail.com>
2021-02-25 15:33:30 -0600

    roll&list

--- src/index.js
+++ src/index.js
@@ -8,34 +8,37 @@ let rollBackward = document.querySelector(".slides-control--prev")
 const absoluteIndex = (index,arr) => index < 0 ? arr.length + index: index
 
 rollForward.addEventListener("click", () => {
-  console.log("click forward")
-  console.log(slides)
+  // console.log("click forward")
+  // console.log(slides)
   slideIndex = ++slideIndex % slides.length;
-  console.log(slideIndex)
-  console.log(absoluteIndex(slideIndex,slides))
-  console.log(absoluteIndex(slideIndex-1,slides))
+  // console.log(slideIndex)
+  // console.log(absoluteIndex(slideIndex,slides))
+  // console.log(absoluteIndex(slideIndex-1,slides))
   slides[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
   slides[absoluteIndex(slideIndex-1,slides)].classList.toggle("slides__slide--show");
 })
 
 rollBackward.addEventListener("click", () => {
-  console.log("click backward")
-  console.log(slides)
   slideIndex = --slideIndex % slides.length;
-  console.log(slideIndex)
-  console.log(absoluteIndex(slideIndex,slides))
-  console.log(absoluteIndex(slideIndex+1,slides))
   slides[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
   slides[absoluteIndex(slideIndex+1,slides)].classList.toggle("slides__slide--show");
 })
 
 
-let dot1 = document.querySelector(".dots__dot1")
-let dot2 = document.querySelector(".dots__dot2")
-let dot3 = document.querySelector(".dots__dot3")
-let dot4 = document.querySelector(".dots__dot4")
+let rollingDots = document.querySelectorAll(".dot")
+let imagesList = document.querySelectorAll(".images-list__item")
 
-dot2.addEventListener("click", () => {
+const ImageSelect = index =>{
+  slides.forEach(slide => slide.classList.remove("slides__slide--show"));
+  slides[1].classList.toggle("slides__slide--show");
+}
+
+rollingDots[1].addEventListener("click", () => {
+  slides.forEach(slide => slide.classList.remove("slides__slide--show"));
+  slides[1].classList.toggle("slides__slide--show");
+})
+
+imagesList[1].addEventListener("click", () => {
   slides.forEach(slide => slide.classList.remove("slides__slide--show"));
   slides[1].classList.toggle("slides__slide--show");
 })
