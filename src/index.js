df0955d3da7341856fd31d01c4628638bfbcae95
Kenyg369 <kengrand369@gmail.com>
2021-04-06 21:04:31 -0500

    gundamIIdam

--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
 
-let slideIndex = 0
-let slides = document.querySelectorAll(".slides__slide");
+let previousIndex = 0;
+let currentIndex = 0;
+
+let imagesList = document.querySelectorAll(".images-list__item");
+let showcase = document.querySelectorAll(".slides__slide");
+let rollingDots = document.querySelectorAll(".dot");
+
+updateSlides(currentIndex);
+
+// Main-function
+
+function updateSlides(idx){
+  previousIndex = currentIndex
+  currentIndex = idx
+  updateImageList()
+  updateShowcase()
+  updateDots()
+
+}
+// Sub-function 
+function updateImageList(){
+  for (let [idx, image] of imagesList.entries()) {
+    image.addEventListener("click",()=>{
+      imagesList[idx].classList.toggle("images-list__item--hightlight");
+      imagesList[previousIndex].classList.toggle("images-list__item--hightlight");
+
+      updateSlides(idx);
+    })
+  }
+}
+
+function updateShowcase(){
+  for (let [idx, showcase] of showcase.entries()) {
+    showcase.addEventListener("click",()=>{
+      showcase[idx].classList.toggle("slides__slide--show");
+      showcase[previousIndex].classList.toggle("slides__slide--show");
+      
+      updateSlides(idx);
+    })
+  }
+}
+
+function updateDots(){
+  for (let [idx, dot] of rollingDots.entries()) {
+    dot.addEventListener("click",()=>{
+      dot[idx].classList.toggle("dot--active");
+      dot[previousIndex].classList.toggle("dot--active");
+
+      updateSlides(idx);
+    })
+  }
+}
+
+
+ // Roll Forward & Roll Backward
 
 let rollForward = document.querySelector(".slides-control--next")
 let rollBackward = document.querySelector(".slides-control--prev")
@@ -8,37 +61,13 @@ let rollBackward = document.querySelector(".slides-control--prev")
 const absoluteIndex = (index,arr) => index < 0 ? arr.length + index: index
 
 rollForward.addEventListener("click", () => {
-  // console.log("click forward")
-  // console.log(slides)
   slideIndex = ++slideIndex % slides.length;
-  // console.log(slideIndex)
-  // console.log(absoluteIndex(slideIndex,slides))
-  // console.log(absoluteIndex(slideIndex-1,slides))
-  slides[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
-  slides[absoluteIndex(slideIndex-1,slides)].classList.toggle("slides__slide--show");
+  showcase[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
+  showcase[absoluteIndex(slideIndex-1,slides)].classList.toggle("slides__slide--show");
 })
 
 rollBackward.addEventListener("click", () => {
   slideIndex = --slideIndex % slides.length;
-  slides[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
-  slides[absoluteIndex(slideIndex+1,slides)].classList.toggle("slides__slide--show");
-})
-
-
-let rollingDots = document.querySelectorAll(".dot")
-let imagesList = document.querySelectorAll(".images-list__item")
-
-const ImageSelect = index =>{
-  slides.forEach(slide => slide.classList.remove("slides__slide--show"));
-  slides[1].classList.toggle("slides__slide--show");
-}
-
-rollingDots[1].addEventListener("click", () => {
-  slides.forEach(slide => slide.classList.remove("slides__slide--show"));
-  slides[1].classList.toggle("slides__slide--show");
-})
-
-imagesList[1].addEventListener("click", () => {
-  slides.forEach(slide => slide.classList.remove("slides__slide--show"));
-  slides[1].classList.toggle("slides__slide--show");
+  showcase[absoluteIndex(slideIndex,slides)].classList.toggle("slides__slide--show");
+  showcase[absoluteIndex(slideIndex+1,slides)].classList.toggle("slides__slide--show");
 })
